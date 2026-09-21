bc00da0beaebc39aff88c247f49d9e0233af404d
Lusoris <lusoris@pm.me>
2026-09-21 09:06:33 +0200

    fix(native): replace unbounded string writes

--- compat/python-vmaf/matlab/strred/matlabPyrTools/MEX/innerProd.c
+++ compat/python-vmaf/matlab/strred/matlabPyrTools/MEX/innerProd.c
@@ -19,31 +19,52 @@ void mexFunction(int nlhs,             /* Num return vals on lhs */
                  const mxArray *prhs[] /* Matrices on rhs */
 )
 {
-    register double *res, *mat, tmp;
-    register int len, wid, i, k, j, jlen, ilen, imat, jmat;
-    mxArray *arg;
+    double *res;
+    double *mat;
+    double tmp;
+    size_t len;
+    size_t wid;
+    size_t i;
+    size_t k;
+    size_t j;
+    size_t jlen;
+    size_t ilen;
+    size_t imat;
+    size_t jmat;
+    const mxArray *arg;
+
+    (void)nlhs;
+    (void)nrhs;
 
     /* get matrix input argument */
     /* should be matrix in which num rows >= num columns */
     arg = prhs[0];
     mat = mxGetPr(arg);
-    len = (int)mxGetM(arg);
-    wid = (int)mxGetN(arg);
-    if (wid > len)
-        printf("innerProd: Warning: width %d is greater than length %d.\n", wid, len);
-    plhs[0] = (mxArray *)mxCreateDoubleMatrix(wid, wid, mxREAL);
-    if (plhs[0] == NULL)
-        mexErrMsgTxt(sprintf("Error allocating %dx%d result matrix", wid, wid));
+    len = mxGetM(arg);
+    wid = mxGetN(arg);
+    if (wid > len) {
+        (void)printf("innerProd: Warning: width %zu is greater than length %zu.\n", wid, len);
+    }
+    plhs[0] = mxCreateDoubleMatrix(wid, wid, mxREAL);
+    if (plhs[0] == NULL) {
+        char message[80];
+        const int written =
+            snprintf(message, sizeof(message), "Error allocating %zux%zu result matrix", wid, wid);
+        if (written < 0 || (size_t)written >= sizeof(message)) {
+            mexErrMsgTxt("Error allocating result matrix");
+        }
+        mexErrMsgTxt(message);
+    }
     res = mxGetPr(plhs[0]);
 
     for (i = 0, ilen = 0; i < wid; i++, ilen += len) {
         for (j = i, jlen = ilen; j < wid; j++, jlen += len) {
             tmp = 0.0;
-            for (k = 0, imat = ilen, jmat = jlen; k < len; k++, imat++, jmat++)
+            for (k = 0, imat = ilen, jmat = jlen; k < len; k++, imat++, jmat++) {
                 tmp += mat[imat] * mat[jmat];
+            }
             res[i * wid + j] = tmp;
             res[j * wid + i] = tmp;
         }
     }
-    return;
 }
